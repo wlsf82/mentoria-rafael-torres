bd5f9c81e8e3bda16870501293c1228d334adf02
Walmyr <wlsf82@gmail.com>
2018-12-02 15:11:04 +0100

    Add new tests and comment other tests

--- tests/specs/homepage.spec.js
+++ tests/specs/homepage.spec.js
@@ -11,6 +11,22 @@ describe('Home page', () => {
     helper.waitForElementVisibility(createButton)
   })
 
+  it('go to create video page when clicking create button', () => {
+    const createButton = element(by.className('add-video-button'))
+
+    helper.clickWhenClickable(createButton)
+
+    helper.waitForUrlToBeEqualToExpectedUrl(`${browser.baseUrl}/videos/create`)
+  });
+
+  it('shows two videos in the home page', () => {
+    const videoCards = element.all(by.className('video-card'))
+
+    helper.waitForElementVisibility(videoCards.last())
+
+    expect(videoCards.count()).toBe(2)
+  })
+
   it('should valid message', () => {
     const buttonRegister = element(by.id('submit-button'))
     const createButton = element(by.className('add-video-button'))
@@ -20,37 +36,37 @@ describe('Home page', () => {
     expect(element(by.className('error')).getText()).toEqual('Title is required')
   })
 
-  it('should save a video', () => {
-    const fieldtitle = element(by.id('video-title-input'))
-    const fieldDescription = element(by.id('video-description-input'))
-    const fieldURL = element(by.id('video-url-input'))
-    const buttonRegister = element(by.id('submit-button'))
-    const createButton = element(by.className('add-video-button'))
+  // it('should save a video', () => {
+  //   const fieldtitle = element(by.id('video-title-input'))
+  //   const fieldDescription = element(by.id('video-description-input'))
+  //   const fieldURL = element(by.id('video-url-input'))
+  //   const buttonRegister = element(by.id('submit-button'))
+  //   const createButton = element(by.className('add-video-button'))
 
-    helper.clickWhenClickable(createButton)
-    helper.fillFieldWithTextWhenVisible(fieldtitle, 'Title Tests 001')
-    helper.fillFieldWithTextWhenVisible(fieldDescription, 'Description of videos')
-    helper.fillFieldWithTextWhenVisible(fieldURL, 'https://www.youtube.com/watch?v=M76qUQTt_Sw')
-    helper.clickWhenClickable(buttonRegister)
-  })
+  //   helper.clickWhenClickable(createButton)
+  //   helper.fillFieldWithTextWhenVisible(fieldtitle, 'Title Tests 001')
+  //   helper.fillFieldWithTextWhenVisible(fieldDescription, 'Description of videos')
+  //   helper.fillFieldWithTextWhenVisible(fieldURL, 'https://www.youtube.com/watch?v=M76qUQTt_Sw')
+  //   helper.clickWhenClickable(buttonRegister)
+  // })
 
-  it('should change a video', () => {
-    const selectEdit = element(by.css('div.video-title'))
-    const buttonEdit = element(by.id('edit'))
-    const fieldtitle = element(by.id('video-title-input'))
-    const buttonSave = element(by.className('edit-video-button'))
+  // it('should change a video', () => {
+  //   const selectEdit = element(by.css('div.video-title'))
+  //   const buttonEdit = element(by.id('edit'))
+  //   const fieldtitle = element(by.id('video-title-input'))
+  //   const buttonSave = element(by.className('edit-video-button'))
     
-    helper.clickWhenClickable(selectEdit)
-    helper.clickWhenClickable(buttonEdit)
-    helper.clearFieldWhenVisibleAndFillItWithText(fieldtitle, 'Change successfully')
-    helper.clickWhenClickable(buttonSave)
-  })
+  //   helper.clickWhenClickable(selectEdit)
+  //   helper.clickWhenClickable(buttonEdit)
+  //   helper.clearFieldWhenVisibleAndFillItWithText(fieldtitle, 'Change successfully')
+  //   helper.clickWhenClickable(buttonSave)
+  // })
 
-  it('should delete a video', () => {
-    const selectEdit = element(by.css('div.video-title'))
-    const buttonDelete = element(by.id('delete'))
+  // it('should delete a video', () => {
+  //   const selectEdit = element(by.css('div.video-title'))
+  //   const buttonDelete = element(by.id('delete'))
 
-    helper.clickWhenClickable(selectEdit)
-    helper.clickWhenClickable(buttonDelete)
-  })
+  //   helper.clickWhenClickable(selectEdit)
+  //   helper.clickWhenClickable(buttonDelete)
+  // })
 })
