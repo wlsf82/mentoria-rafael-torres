a902316151603e5e91b0f2cdfd5347310c83f206
Rafael Torres dos Santos <rafael.santos@ltm.digital>
2018-12-02 11:30:20 -0200

    Add spec reporter and headless

--- tests/protractor.conf.js
+++ tests/protractor.conf.js
@@ -1,7 +1,27 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter
+
 module.exports.config = {
   baseUrl: 'http://localhost:4001',
   specs: ['./specs/*.spec.js'],
+  capabilities: {
+    browserName: "chrome",
+    chromeOptions: {
+        args: [
+            "--headless",
+            "--disable-gpu",
+            "--window-size=1280,1024"
+        ]
+    }
+},
+
   onPrepare: () => {
     browser.ignoreSynchronization = true
+    
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayFailuresSummary: true,
+      displayFailuredSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true
+    }))
   }
 }
