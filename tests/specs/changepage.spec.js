e68cc9ab8d72fb7c7ab359b0e7d2488f54262d94
Walmyr <wlsf82@gmail.com>
2018-12-09 18:02:56 +0100

    Refactor edit video test suite
    The beforeEach was refactor to access an existing video for edition
    We started implementing page objects (still work in progress)
    We added a missing expectation in one of the exsiting tests
    We implemented a new test case to explore inhiritance in JS
    Also, the code was prettified.

--- tests/specs/changepage.spec.js
+++ tests/specs/changepage.spec.js
@@ -1,16 +1,31 @@
 'use strict'
 const helper = require('protractor-helper')
 
+const EditPage = require('../page-objects/editPage')
+
 describe('Change page', () => {
-  beforeEach(() => browser.get('/videos/5c040691e67e122758c69042/edit'))
+  let editPage
+
+  beforeEach(() => {
+    editPage = new EditPage()
+
+    const firstVideoCardLink = element.all(by.css('.video-card a')).first()
+    const editVideoButton = element(by.className('edit-video-button'))
+
+    browser.get('/videos/')
+    helper.clickWhenClickable(firstVideoCardLink)
+    helper.clickWhenClickable(editVideoButton)
+  })
 
   it('should change a video', () => {
-    
-    const fieldtitle = element(by.id('video-title-input'))
-    const buttonSave = element(by.className('edit-video-button'))
-    
-    helper.clearFieldWhenVisibleAndFillItWithText(fieldtitle, 'Change successfully')
-    helper.clickWhenClickable(buttonSave)
+    const newVideoTitle = 'Change successfully'
+
+    const videoTitle = element(by.className('video-title'))
+
+    helper.clearFieldWhenVisibleAndFillItWithText(editPage.titleField, newVideoTitle)
+    helper.clickWhenClickable(editPage.saveButton)
+
+    helper.waitForTextToBePresentInElement(videoTitle, newVideoTitle)
   })
 
   it('should show a delete button and a edit button', () => {
@@ -19,5 +34,13 @@ describe('Change page', () => {
 
     helper.waitForElementVisibility(deleteButton)
     helper.waitForElementVisibility(editButton)
+  })
+
+  it('goes to home page when clicking in the logo', () => {
+    helper.clickWhenClickable(editPage.logo)
+
+    helper.waitForUrlToBeEqualToExpectedUrl(`${browser.baseUrl}/videos`)
+
+    browser.sleep(2000)
   })
 })
